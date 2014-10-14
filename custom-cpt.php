5bb81e8ddc30645d2a8e18e4d3ab313da8ae0222
khromov <stanislav.khromov@gmail.com>
2014-10-15 01:08:32 +0200

    When development mode constant is not set, assume it's the production environment

--- custom-cpt.php
+++ custom-cpt.php
@@ -11,7 +11,8 @@ License: GPL2
 
 add_action('plugins_loaded', function()
 {
-	if(defined('CUSTOM_CPT_DEVELOPMENT_MODE') && CUSTOM_CPT_DEVELOPMENT_MODE !== true)
+	/* If nothing defined in config, load our CPT:s because we assume it's a regular user installing on a clean site */
+	if(!defined('CUSTOM_CPT_DEVELOPMENT_MODE') || (defined('CUSTOM_CPT_DEVELOPMENT_MODE') && (CUSTOM_CPT_DEVELOPMENT_MODE !== true)))
 	{
 		foreach (glob(__DIR__ . "/cpt/*.php") as $filename)
 			include $filename;
